02c53c9e8d8498e70be0d75ba9aae3b2a7866d8e
satha-1 <137098235+satha-1@users.noreply.github.com>
2025-02-10 17:20:35 +0530

    error fixing in register user

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,10 +1,10 @@
 const User=require("../models/userModel");
 const createUser= async (req, res) => {
     const email = req.body.email;
-        const findUser= await User.findOne(email);
+        const findUser= await User.findOne({email:email});
         if(!findUser){
             //create a new user
-            const newUser = User.create(req.body);
+            const newUser = await User.create(req.body);
             res.json(newUser);
         }
         else{
